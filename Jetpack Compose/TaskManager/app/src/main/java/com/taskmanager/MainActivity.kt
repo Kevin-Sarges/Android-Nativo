c4874e2f823dfca5f0b7b0ac9eb387d1d96aafe4
Kevin-Sarges <kevinsarges08@gmail.com>
2024-01-05 14:38:02 -0300

    taskmanager

--- Jetpack Compose/TaskManager/app/src/main/java/com/taskmanager/MainActivity.kt
+++ Jetpack Compose/TaskManager/app/src/main/java/com/taskmanager/MainActivity.kt
@@ -8,8 +8,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ComponentCenter()
                 }
             }
         }
@@ -30,17 +39,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Image() {
+    val image = painterResource(R.drawable.ic_task_completed)
+
+    Image(painter = image, contentDescription = null, contentScale = ContentScale.Fit)
+}
+
+@Composable
+fun TitleText(title: String) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = title,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
     )
 }
 
-@Preview(showBackground = true)
+@Composable
+fun SubTitleText(subTitle: String) {
+    Text(
+        text = subTitle,
+        fontSize = 16.sp,
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Composable
+fun ComponentCenter(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Image()
+        TitleText(title = "All tasks completed")
+        SubTitleText(subTitle = "Nice work!")
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     TaskManagerTheme {
-        Greeting("Android")
+        ComponentCenter()
     }
 }
